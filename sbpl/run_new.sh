d4f204852f4da9c42fe14067dbf42aca2b55671c
Laxman Dhulipala <ldhulipa@andrew.cmu.edu>
2015-10-30 15:04:45 -0400

    temp remove sbpl

--- sbpl/run_new.sh
+++
@@ -1,1 +0,0 @@
-/usr/bin/time -v ./bin/test_para env_examples/nav3d/cubicle-25mm-inflated-env.cfg matlab/mprim/pr2.mprim
--- sbpl/src/planners/PWSA/pwsa_constants.h
+++
@@ -1,5 +0,0 @@
-#ifndef PARA_CONST
-#define PARA_CONST
-#define NUM_THREADS 4
-#define INITIAL_EPS 5
-#endif
