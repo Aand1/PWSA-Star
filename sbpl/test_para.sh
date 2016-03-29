20a0123505b5ad5ea5c5792204d0fdd8cf8a08de
Laxman Dhulipala <ldhulipa@andrew.cmu.edu>
2016-03-29 00:39:56 -0400

    remove spbl

--- sbpl/test_para.sh
+++
@@ -1,7 +0,0 @@
-#./bin/test_para env_examples/nav3d/cubicle-25mm-inflated-env.cfg matlab/mprim/pr2.mprim
-#./bin/test_para env_examples/nav3d/env1.cfg matlab/mprim/pr2.mprim
-gdb -ex run --args ./bin/test_para env_examples/nav3d/env0.cfg matlab/mprim/pr2.mprim
-
-
-#./bin/test_para env_examples/nav2d/env1.cfg 
-#./bin/test_para env_examples/nav2d/env2.cfg 
